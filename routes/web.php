55eba9909bfcc360ae7b756e17ffd9db3dcd295e
anggifitriaa <anggi_229@smkassalaambandung.sch.id>
2021-12-22 16:19:09 +0700

    pkl

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,15 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes([
+    'register' => false, // menghilangkan fitur regis
+    'reset' => false, // menghilangkan fitur logout
+]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
